6ec9852f534b080827231eec515d47d52241053f
Paulo Mateus <39471029+pmateuslourenco@users.noreply.github.com>
2022-02-17 19:52:38 -0300

    Solucionando problemas para exercitar.

---
+++ src/com/exe/Main.java
@@ -0,0 +1,8 @@
+package com.exe;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/exe/OlaMundo.java
@@ -0,0 +1,7 @@
+package com.exe;
+
+public class OlaMundo {
+    public static void main (String[] args){
+        System.out.println("Olá, Mundo!");
+    }
+}
