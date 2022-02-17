559a2670981919d68955d0f4a39d082e10ef24d5
Paulo Mateus <39471029+pmateuslourenco@users.noreply.github.com>
2022-02-17 20:12:33 -0300

    Solucionando problemas para exercitar.

--- src/com/exe/OlaMundo.java
+++ src/com/exe/OlaMundo.java
@@ -2,6 +2,6 @@ package com.exe;
 
 public class OlaMundo {
     public static void main (String[] args){
-        System.out.println("Olá,Mundo!");
+        System.out.println("Olá, Mundo!");
     }
 }
